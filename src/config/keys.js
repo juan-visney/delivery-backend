d3df6717fd12511c3368f6d7f8d1c1621a6635ea
Juan Visney <jv575juan@gmail.com>
2022-03-22 12:52:58 -0400

    test04

--- src/config/keys.js
+++ src/config/keys.js
@@ -3,11 +3,11 @@ env.config()
 
 module.exports = {
     database: {
-        host: process.env.DATABASE_HOST || 'localhost',
-        user: process.env.DATABASE_USER || 'root',
-        password: process.env.DATABASE_PASSWORD || 'jvcode',
+        host: process.env.DATABASE_HOST || 'db-mysql-do-user-11056291-0.b.db.ondigitalocean.com',
+        user: process.env.DATABASE_USER || 'jvcode',
+        password: process.env.DATABASE_PASSWORD || 'qNqDPnwJ0yzhrjUx',
         database: process.env.DATABASE_NAME || 'delivery',
-        port: process.env.DATABASE_PORT
+        port: process.env.DATABASE_PORT || '25060'
     },
     port: process.env.PORT || 4000
 }
